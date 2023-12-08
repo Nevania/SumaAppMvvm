0b03367c38c26a529827cfc484b247a842506d5e
Nevania <140127237+Nevania@users.noreply.github.com>
2023-12-08 22:21:23 +0100

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SumaAppMvvm
+{
+   
+        public partial class MainPage : ContentPage
+        {
+            public MainPage()
+            {
+                InitializeComponent();
+                BindingContext = new MainPageViewModel();
+            }
+
+            private void Sumar_Clicked(object sender, EventArgs e)
+            {
+                ((MainPageViewModel)BindingContext).Sumar();
+            }
+
+            private void Limpiar_Clicked(object sender, EventArgs e)
+            {
+                ((MainPageViewModel)BindingContext).Limpiar();
+            }
+        }
+
+}
+
+
---
+++ MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaAppMvvm
+{
+    internal class MainPageViewModel 
+    {
+        public string Sumar1 { get; set; }
+        public string Sumar2 { get; set; }
+        public string Resultado { get; set; }
+
+        public void Sumar()
+        {
+            int Sumar1, Sumar2, resultado;
+            int.TryParse(Sumar1, out Sumar1);
+            int.TryParse(Sumar2, out Sumar2);
+            resultado = Sumar1 + Sumar2;
+            Resultado = resultado.ToString();
+        }
+
+        public void Limpiar()
+        {
+            Sumar1 = "";
+            Sumar2 = "";
+            Resultado = "";
+        }
+    }
+}
+
